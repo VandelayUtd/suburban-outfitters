0910fd23a01bad2cd351cdf918c55a0a1af8878f
VandelayUtd <drewhadley.guitar@gmail.com>
2022-03-01 10:29:59 -0500

    updated cart data and cart-item add_to_cart function

--- src/components/collection-item/collection-item.jsx
+++ src/components/collection-item/collection-item.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import CustomButton from '../custom-button/custom-button';
+import { addItem } from '../../redux/cart/cart.actions';
 
 import './collection-item.styles.scss'
 
-export const CollectionItem = ({ id, name, price, imageUrl }) => (
-  <div key={id} className='collection-item'>
+const CollectionItem = ({ item, addItem }) => {
+  const {  name, price, imageUrl } = item;
+  return (
+  <div className='collection-item'>
       <div 
         className='image'
         style={{
@@ -16,6 +20,12 @@ export const CollectionItem = ({ id, name, price, imageUrl }) => (
             <span className='name'>{ name }</span>
             <span className='price'>{ price }</span>
         </div>
-        <CustomButton inverted>Add to Cart</CustomButton>
+        <CustomButton onClick={()=> addItem(item)} inverted>Add to Cart</CustomButton>
   </div>
-)
+)}
+
+const mapDispatchToProps = dispatch => ({
+  addItem: item => dispatch(addItem(item))
+})
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
--- src/components/preview-collection/preview-collection.jsx
+++ src/components/preview-collection/preview-collection.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { CollectionItem } from '../collection-item/collection-item';
+import CollectionItem from '../collection-item/collection-item';
 
 import './preview-collection.styles.scss';
 
@@ -7,9 +7,10 @@ export const PreviewCollection = ({ title, items }) => (
   <div className='preview-collection'>
     <h1 className='title'>{title}</h1>
     <div className='preview'>
-        {items.filter((item, index) => index < 4)
-        .map(({id, ...otherItemProps}) => (
-            <CollectionItem key={id} {...otherItemProps}/>
+        {items
+        .filter((item, index) => index < 4)
+        .map(item => (
+            <CollectionItem key={item.id} item={item} />
         ))}
     </div>
   </div>
