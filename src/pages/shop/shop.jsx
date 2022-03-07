bb75c0a011239830d1fa44df16f18d1eb9ff13e2
VandelayUtd <drewhadley.guitar@gmail.com>
2022-03-07 11:23:06 -0500

    moved shop.data into redux store

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,24 +1,23 @@
-import React, { Component } from 'react';
-import SHOP_DATA from './shop.data';
+import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
 import { PreviewCollection } from '../../components/preview-collection/preview-collection';
 
-export default class ShopPage extends Component {
+import { selectCollections } from '../../redux/shop/shop.selectors';
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            collections: SHOP_DATA
-        }
-    }
-
-  render() {
-    const {collections} = this.state
-    return (<div className='shop-page' >
+const ShopPage = ({ collections }) => (
+    <div className='shop-page' >
         {
             collections.map(({id, ...otherCollectionProps }) => (
                 <PreviewCollection key={id} {...otherCollectionProps} />
             ))
         }
-    </div>)
-  }
-}
+    </div>
+)
+
+const mapStateToProps = createStructuredSelector({
+    collections: selectCollections
+})
+
+export default connect(mapStateToProps)(ShopPage)
