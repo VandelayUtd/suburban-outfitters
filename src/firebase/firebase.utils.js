3c33eaa9984e8a0a062671cb4cf957c8ad914abe
VandelayUtd <drewhadley.guitar@gmail.com>
2022-04-06 10:54:30 -0400

    refactored firebase, signin with google functional

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -28,6 +28,22 @@ export const createUserDocumentFromAuth = async (userAuth) => {
   const userDocRef = doc(db, 'users', userAuth.uid);
 
   const userSnapshot = await getDoc(userDocRef);
+
+  if(!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt
+      });
+    } catch (error) {
+      console.log('error creating user', error.message)
+    }
+  }
+  return userDocRef
 } 
 
 
