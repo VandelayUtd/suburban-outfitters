32e773535a47fd8da894eec3aaa2c0771986e118
VandelayUtd <drewhadley.guitar@gmail.com>
2022-04-13 16:06:26 -0400

    added collectionPreview component

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,20 +1,27 @@
 import React, { useContext } from 'react';
 
-import ProductCard from '../../components/product-card/product-card';
-import { ProductsContext } from '../../contexts/products.context';
+import CategoryPreview from '../../components/category-preview/category-preview';
+
+import { CategoriesContext } from '../../contexts/categories.context';
 
 import './shop.styles.scss'
 
 const Shop = () => {
 
-    const { products } = useContext(ProductsContext)
-  
-    return (  
-    <div className='products-container' >
-        {products.map((product) => (
-            <ProductCard key={product.id} product={product} />
-      ))}
-    </div>
-)}
+    const { categoriesMap } = useContext(CategoriesContext)
+
+    return ( 
+        <div className='shop-container'>
+        {
+        Object.keys(categoriesMap).map(title => {
+            const products = categoriesMap[title];
+            return (
+                <CategoryPreview title={title} products={products}/>
+            )
+        }) 
+        }
+        </div>
+    )
+}
 
 export default Shop;
